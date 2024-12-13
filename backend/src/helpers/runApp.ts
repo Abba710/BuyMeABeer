e92707142befc9365e8b0942aec8eebc628c2718
Abba710 <Abbasaifer3@gmail.com>
2024-12-13 18:46:34 +0500

    Prepared the project for work

--- backend/src/helpers/runApp.ts
+++ backend/src/helpers/runApp.ts
@@ -1,34 +1,20 @@
-import Koa from 'koa'
-import Router from 'koa-router'
-import bodyParser from 'koa-bodyparser'
-import cors from '@koa/cors'
-import { Server } from 'http'
-import { bootstrapControllers } from 'amala'
-import { resolve } from 'path'
+import express, { Request, Response, Application } from 'express'
 import env from '@/helpers/env'
 
-const app = new Koa()
+// Create express app
+export default function runApp(): Application {
+  const app: Application = express()
+  const PORT = env.PORT
 
-export default async function () {
-  const router = new Router()
-  await bootstrapControllers({
-    app,
-    basePath: '/',
-    controllers: [resolve(__dirname, '../controllers/*')],
-    disableVersioning: true,
-    router,
+  app.use(express.json())
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
   })
-  app.use(cors({ origin: '*' }))
-  app.use(bodyParser())
-  app.use(router.routes())
-  app.use(router.allowedMethods())
-  return new Promise<Server>((resolve, reject) => {
-    const connection = app
-      .listen(env.PORT)
-      .on('listening', () => {
-        console.log(`HTTP is listening on ${env.PORT}`)
-        resolve(connection)
-      })
-      .on('error', reject)
+
+  app.listen(PORT, () => {
+    console.log(`Сервер запущен на http://localhost:${PORT}`)
   })
+
+  return app
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from 'preact/compat'
-import UserCount from 'components/UserCount'
 
 export default function () {
   return (
     <div className="container prose mx-auto max-w-prose p-10">
       <h1>Frontend template</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <UserCount />
-      </Suspense>
+      <Suspense fallback={<p>Loading...</p>}></Suspense>
     </div>
   )
 }
